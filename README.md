6d7074238df213447bb2667cb74d4776d16d3332
Lyon Huang <lyon.huang@prophetdigits.com>
2021-01-22 21:46:10 +0800

    modify webpack

--- README.md
+++ README.md
@@ -1,2 +1,43 @@
 # github_user_search
 User search bar to search github user and show the information about their repositories, followers, followings, ...etc.
+
+
+# Github User Search
+
+Search github users and view their information about their repositories, followers, followings, ...etc.
+
+
+## How it works?
+
+It built with React and other tools to search github users and
+view their information. It use Redux to controlled the props and state.
+The CSS style is built by styled-component and styled-system.
+
+Depoly with Heroku
+
+Feel free to open issues for questions/improvements!
+
+#### Libraries and tools
+
+* Babel
+* Webpack
+* React
+* React Router
+* Redux
+* Redux thunk
+* styled-component
+* styled-system
+
+
+
+## How to Run
+
+1. Clone the repository
+2. Install the dependences `npm install`
+3. Run the server `npm run start`
+4. Visit `http://localhost:3000/#/search`
+
+## Deployed in Heroku
+
+Visit `https://lyon-github-search-user.herokuapp.com/#/search`
+
--- webpack.prod.js
+++ webpack.prod.js
@@ -1,54 +1,14 @@
 const webpack = require('webpack');
-const HtmlWebpackPlugin = require('html-webpack-plugin')
+const merge = require('webpack-merge');
+const common = require('./webpack.common.js');
 
-module.exports = {
-	entry: './app/main.js',
-	output: {
-		filename: './dist/bundle.[hash].js',
-		path:  __dirname + '/',
-	},
-	resolve: {
-		modules: [
-			"node_modules",
-		],
-		extensions: [".js", ".jsx"],
-	},
-	module: {
-		rules: [
-			{
-				test: /\.js$/,
-				exclude: /(node_modules)/,
-				use: {
-					loader: 'babel-loader',
-					options: {
-						presets: ['es2015', 'react'],
-						plugins: [
-							["import", { libraryName: "antd", style: "css", }],
-							"transform-class-properties",
-							"transform-object-rest-spread",
-							"react-hot-loader/babel",
-							"syntax-dynamic-import"
-						]
-					}
-				}
-			},
-			{
-				test: /\.css$/,
-				use: [
-					{ loader: "style-loader" },
-					{ loader: "css-loader" }
-				]
-			}
-		]
-	},
-	devtool: 'cheap-module-eval-source-map',
-	plugins: [
-		new webpack.DefinePlugin({
-			ENV: JSON.stringify("production")
-		}),
-		new HtmlWebpackPlugin({
-			filename: 'index.html',
-			template: './template.html'
-		})
-	]
-};
+module.exports = merge(common, {
+  plugins: [
+    new webpack.DefinePlugin({
+      'process.env': {
+        'NODE_ENV': JSON.stringify('production')
+      },
+      ENV: JSON.stringify("prod")
+    })
+  ]
+});
